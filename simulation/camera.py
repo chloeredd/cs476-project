fde2100c1bad9dc0149923778b58647029cc50b8
seth Jayasena <sethayasena@MacBook-Pro.local>
2025-11-23 16:35:14 -0500

    Updated camera.py

--- simulation/camera.py
+++ simulation/camera.py
@@ -1,34 +1,44 @@
 import pybullet as p
+import numpy as np
+import yaml
 
-class Needle:
-    '''
-    This class represents a needle-like object in the simulation world
-    '''
+class Camera:
+    def __init__(self, drone):
+        self.drone = drone
 
-    def __init__(self, position):
+        with open("configs/simulation_config.yaml") as f:
+            config = yaml.safe_load(f)
 
-        #Visual shape: yellow cylinder
-        visual = p.createVisualShape(
-            #It's a cylinder
-            p.GEOM_CYLINDER,
-            radius = 0.004,
-            length = 0.1,
-            #It has a yellow color
-            rgbaColor = [1, 1, 0, 1]
+        camera = config["camera"]
+        self.w = camera["width"]
+        self.h = camera["height"]
+        self.fov = camera["fov"]
+        self.near = camera["near"]
+        self.far = camera["far"]
+
+    def getFrame(self):
+        #Get the position, orientation, and rotation of the drone
+        position, orientation = p.getBasePositionAndOrientation(self.drone.body)
+        rotation = p.getMatrixFromQuaternion(orientation)
+
+        eye = position
+        #Facing downwards
+        target = [position[0] + rotation[0], position[1] + rotation[3], position[2] - 1] 
+
+        #computeViewMatrix transforms world coordinates into camera
+        #coordinates 
+        view = p.computeViewMatrix(eye, target, [0, 0, 1])
+        #generate the camera's intrinsic matrix 
+        #this is used for rendering images in the simulation 
+        projection = p.computeProjectionMatrixFOV(self.fov, 1.0, self.near, self.far)
+
+        #capture an image from a virtual camera in the simulator
+        _, _, rgb, _, seg = p.getCameraImage(
+            width = self.w,
+            height = self.h,
+            viewMatrix = view,
+            projectionMatrix = projection,
+            renderer = p.ER_BULLET_HARDWARE_OPENGL
         )
-        
-        #Collision shape is the same as the visual shape
-        collision = p.createCollisionShape(
-            p.GEOM_CYLINDER,
-            radius = 0.004,
-            height = 0.1
-        )
 
-        #Create the object in simulation
-        self.body = p.createMultiBody(
-            baseMass = 0.01,
-            baseCollisionShapeIndex = collision,
-            baseVisualShapeIndex = visual,
-            basePosition = position
-        )
-
+        return np.array(rgb), np.array(seg)
